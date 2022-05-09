b637bd77b9a679295f32b18e10b5502aafdbd0dd
Juan Manuel Oubiña <juan.manuel.oubina@gmail.com>
2022-05-09 13:46:08 -0300

    Move pagination logic to Base DAO

--- app/repositories/base_transactional_dao.py
+++ app/repositories/base_transactional_dao.py
@@ -1,8 +1,14 @@
+from typing import Any, List, TypeVar, Callable, Mapping
+
 from databases import Database
 from databases.core import Transaction
+from pydantic import ValidationError
+from sqlalchemy import asc, desc
 
-from .exceptions import TransactionAlreadyStartedException
+from .exceptions import TransactionAlreadyStartedException, DatabaseParseException, DatabaseException
+from ..domain import Page, PageRequest, NEXT_PAGE_PREFIX, PREVIOUS_PAGE_PREFIX
 
+T = TypeVar('T')
 
 class BaseTransactionalDAO:
     def __init__(self, db: Database, tx: Transaction = None):
@@ -28,3 +34,62 @@ class BaseTransactionalDAO:
 
         await self.tx.rollback()
         self.tx = None
+
+    async def pagination_query(
+            self, query, order_column, order_column_cursor_value, page_request: PageRequest, row_mapping_fn: Callable[[Mapping], T]
+    ) -> Page[T]:
+        if page_request.is_next_cursor():
+            query = query.where(order_column > order_column_cursor_value).order_by(asc(order_column)).limit(
+                page_request.size + 1)
+        elif page_request.is_previous_cursor():
+            query = query.where(order_column < order_column_cursor_value).order_by(desc(order_column)).limit(
+                page_request.size + 1)
+        else:
+            query = query.order_by(asc(order_column)).limit(page_request.size + 1)
+
+        try:
+            rows = await self.db.fetch_all(query)
+            data = [row_mapping_fn(row) for row in rows]
+        except ValidationError as vex:
+            raise DatabaseParseException(str(vex))
+        except Exception as ex:
+            raise DatabaseException(str(ex))
+
+        data.sort(key=lambda user: user.id)
+        data_size = len(data)
+
+        return_data = _calculate_data(page_request, data)
+        id_list = [str(user.id) for user in return_data]
+
+        return Page(
+            data=return_data,
+            next_page=_calculate_next_page(page_request, id_list, data_size),
+            previous_page=_calculate_previous_page(page_request, id_list, data_size)
+        )
+
+
+def _calculate_data(page_request: PageRequest, data: List[Any]) -> List[Any]:
+    if len(data) > page_request.size:
+        if page_request.is_previous_cursor():
+            return data[1:]
+        if page_request.is_next_cursor() or page_request.cursor is None:
+            return data[:-1]
+
+    return data
+
+
+def _calculate_next_page(page_request: PageRequest, data_ids: List[str], data_size: int) -> str | None:
+    if page_request.is_previous_cursor() or data_size > page_request.size:
+        return NEXT_PAGE_PREFIX + data_ids[len(data_ids) - 1]
+
+    return None
+
+
+def _calculate_previous_page(page_request: PageRequest, data_ids: List[str], data_size: int) -> str | None:
+    if page_request.cursor is None:
+        return None
+
+    if page_request.is_next_cursor() or data_size > page_request.size:
+        return PREVIOUS_PAGE_PREFIX + data_ids[0]
+
+    return None
--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,14 +1,12 @@
-from typing import Optional
+from typing import Optional, Callable, Mapping
 
 from databases import Database
 from pydantic import ValidationError
-from sqlalchemy import asc, desc
 
 from app.domain import User, UserPrototype, Page, PageRequest
 from app.repositories.dtos import UserDTO
 from .base_transactional_dao import BaseTransactionalDAO
 from .exceptions import DatabaseException, DatabaseParseException
-from .pagination import calculate_next_page_value, calculate_previous_page_value, calculate_data
 
 
 class UserRepository(BaseTransactionalDAO):
@@ -61,40 +59,20 @@ class UserRepository(BaseTransactionalDAO):
 
     # Users are sorted by id. They could be ordered by any other unique and sequential field
     async def get_users(self, page_request: PageRequest) -> Page[User]:
-        query = UserDTO.select()
+        base_query = UserDTO.select()
+        order_column = UserDTO.c.id
+        order_column_cursor_value = int(page_request.get_cursor_value()) if page_request.cursor is not None else None
+        row_mapping_fn: Callable[[Mapping], User] = lambda row: User(
+            id=row.get("id"),
+            first_name=row.get("first_name"),
+            last_name=row.get("last_name"),
+            birth_date=row.get("birth_date")
+        )
 
-        if page_request.is_next_cursor():
-            query = query.where(UserDTO.c.id > int(page_request.get_cursor_value())).order_by(asc(UserDTO.c.id)).limit(
-                page_request.size + 1)
-        elif page_request.is_previous_cursor():
-            query = query.where(UserDTO.c.id < int(page_request.get_cursor_value())).order_by(desc(UserDTO.c.id)).limit(
-                page_request.size + 1)
-        else:
-            query = query.order_by(asc(UserDTO.c.id)).limit(page_request.size + 1)
-
-        data = []
-        try:
-            rows = await self.db.fetch_all(query)
-            for row in rows:
-                data.append(User(
-                    id=row.get("id"),
-                    first_name=row.get("first_name"),
-                    last_name=row.get("last_name"),
-                    birth_date=row.get("birth_date")
-                ))
-        except ValidationError as vex:
-            raise DatabaseParseException(str(vex))
-        except Exception as ex:
-            raise DatabaseException(str(ex))
-
-        data.sort(key=lambda user: user.id)
-        data_size = len(data)
-
-        return_data = calculate_data(page_request, data)
-        id_list = [str(user.id) for user in return_data]
-
-        return Page(
-            data=return_data,
-            next_page=calculate_next_page_value(page_request, id_list, data_size),
-            previous_page=calculate_previous_page_value(page_request, id_list, data_size)
+        return await self.pagination_query(
+            query=base_query,
+            order_column=order_column,
+            order_column_cursor_value=order_column_cursor_value,
+            page_request=page_request,
+            row_mapping_fn=row_mapping_fn
         )
